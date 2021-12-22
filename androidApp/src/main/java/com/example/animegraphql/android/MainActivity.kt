a0843ea9ba0519239142f9b8e7d2865c9a4f3be6
koichi tada <kou26haru@gmail.com>
2021-12-22 23:53:35 +0900

    modify: fix error

--- androidApp/src/main/java/com/example/animegraphql/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/animegraphql/android/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.animegraphql.android
 
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.example.animegraphql.PreferencesManager
 import com.example.animegraphql.apollo.AnimeListClient
-import com.example.animegraphql.android.R
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -16,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main))
+        setContentView(R.layout.activity_main)
         GlobalScope.launch {
             val result = AnimeListClient().getMediaPage()
             Log.d("pokemon", result!!.toString())
