176af0debe2932fd6b5ef4a536dc3237d6a42994
koichi tada <kou26haru@gmail.com>
2021-12-26 00:04:34 +0900

    modify: Composeを導入しました。

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,14 +17,36 @@ android {
             isMinifyEnabled = false
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.0-rc01"
+    }
 }
 
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
     implementation("com.apollographql.apollo3:apollo-normalized-cache:3.0.0")
     implementation("io.insert-koin:koin-android:3.1.4")
+    implementation("androidx.compose.ui:ui:1.1.0-rc01")
+    implementation("androidx.compose.compiler:compiler:1.1.0-rc02")
+    implementation("androidx.compose.foundation:foundation-layout:1.1.0-rc01")
+    implementation("androidx.navigation:navigation-compose:2.4.0-rc01")
+    implementation("androidx.compose.material:material:1.1.0-rc01")
+    implementation("androidx.compose.animation:animation:1.1.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling:1.1.0-rc01")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.1.0-rc01")
 }
